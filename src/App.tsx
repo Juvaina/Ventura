f7e123048987d8a56986c5a9b6a2a4ebc126aaf7
Juvaina <juvaina_mammikkakath_from.isg@hdb.gov.sg>
2025-05-27 15:48:34 +0800

    Sidebar update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import FeatureCard from './components/FeatureCard';
 function App() {
   return (
     <div className='grid grid-cols-[20%_80%]'>
-      <div className='bg-gray-400 h-screen'>Sidebar</div>
+      <div className='bg-gray-400 h-screen'>Sidebar Content</div>
       <main className='max-w-7xl mx-auto'>
         <FeatureCard />
       </main>
