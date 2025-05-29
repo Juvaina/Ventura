982c7430f2626eaa2971d7b79ffc44479bf6a771
Juvaina <juvaina_mammikkakath_from.isg@hdb.gov.sg>
2025-05-29 16:14:28 +0800

    Update offcanvas button styling

--- src/components/OffCanvas.tsx
+++ src/components/OffCanvas.tsx
@@ -1,3 +1,4 @@
+import { IconX } from '@tabler/icons-react';
 import React, { useState } from 'react';
 
 interface OffcanvasProps {
@@ -33,9 +34,10 @@ const Offcanvas: React.FC<OffcanvasProps> = ({ isOpen, onClose }) => {
       >
         <button
           onClick={onClose}
-          className='text-gray-500 hover:text-red-500 text-2xl float-right me-8 p-1 cursor-pointer'
+          className='absolute top-4 right-4 text-gray-400 hover:text-black/70 transition'
+          aria-label='Close'
         >
-          x
+          <IconX size={28} />
         </button>
         <div className='flex flex-col gap-10 w-full p-8'>
           <h2 className='text-2xl font-semibold'>Tell us your Idea!</h2>
@@ -59,7 +61,7 @@ const Offcanvas: React.FC<OffcanvasProps> = ({ isOpen, onClose }) => {
                   onClick={() => handleTopicClick(tag)}
                   className={`border px-3 py-1 rounded-full text-sm cursor-pointer ${
                     selectedTopic === tag
-                      ? 'bg-pink-500 text-white'
+                      ? 'bg-red-700 text-white'
                       : 'bg-gray-100 hover:bg-gray-200'
                   }`}
                 >
@@ -68,7 +70,7 @@ const Offcanvas: React.FC<OffcanvasProps> = ({ isOpen, onClose }) => {
               ))}
             </div>
           </div>
-          <button className='bg-pink-500 text-white px-4 py-2 rounded hover:bg-pink-600 self-start font-medium'>
+          <button className='bg-red-700 text-white px-4 p-2 rounded hover:bg-red-800 self-start cursor-pointer'>
             Submit Idea
           </button>
         </div>
